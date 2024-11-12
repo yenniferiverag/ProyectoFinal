344d47b49e4768ac7b2179d861a9ccf978727fbd
yenniferiverag <riverayennifer48@gmail.com>
2024-11-11 23:02:37 -0600

    update

---
+++ src/grupo2_sc/pkg303_mv_avanceno2/Grupo2_SC303_MV_AvanceNo2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo2_sc.pkg303_mv_avanceno2;
+
+/**
+ *
+ * @author Acer
+ */
+public class Grupo2_SC303_MV_AvanceNo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
