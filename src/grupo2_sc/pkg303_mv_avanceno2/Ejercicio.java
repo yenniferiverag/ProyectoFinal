3ec0fa70c1d9aa9e47df642558eb8a0c661f43d0
yenniferiverag <riverayennifer48@gmail.com>
2024-11-12 22:50:59 -0600

    User class created User class comments added TipoMaquina created TipoEjercicio created GestionUsuarios created GestionRutinas created Ejercicio created

---
+++ src/grupo2_sc/pkg303_mv_avanceno2/Ejercicio.java
@@ -0,0 +1,30 @@
+public class Ejercicio {
+    private String nombre;
+    private String descripcion;
+    private TipoMaquina tipoMaquina;
+    private TipoEjercicio tipoEjercicio;
+    private int repeticiones;
+    private int duracion;
+    private boolean requierePeso;
+    private double peso;
+
+    // Constructor
+    public Ejercicio(String nombre, String descripcion, TipoMaquina tipoMaquina, TipoEjercicio tipoEjercicio, int repeticiones, int duracion, boolean requierePeso, double peso) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.tipoMaquina = tipoMaquina;
+        this.tipoEjercicio = tipoEjercicio;
+        this.repeticiones = repeticiones;
+        this.duracion = duracion;
+        this.requierePeso = requierePeso;
+        this.peso = peso;
+    }
+
+    // Getters and setters for each property
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+}
