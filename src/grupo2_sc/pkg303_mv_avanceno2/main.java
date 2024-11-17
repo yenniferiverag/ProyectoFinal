72dd4dae4d2d69eecabf7037abaed7cd2891a035
yenniferiverag <riverayennifer48@gmail.com>
2024-11-17 17:38:47 -0600

    User class created User class comments added TipoMaquina created TipoEjercicio created GestionUsuarios created GestionRutinas created Ejercicio created

---
+++ src/grupo2_sc/pkg303_mv_avanceno2/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo2_sc.pkg303_mv_avanceno2;
+
+
+public class main {
+    
+    public static void main (String[] args) {
+        
+     FidnessPro FidnessPro2 = new FidnessPro();
+     FidnessPro2.setVisible(true);
+     
+    }
+}
